97b95cdf00fb2c3936ec070debe195d1c4118042
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-07-31 16:56:50 -0400

    basic server setup and route placeholders

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS bearblog_db;
+
+CREATE DATABASE bearblog_db;
+
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    display_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    pass_hash VARCHAR(60) NOT NULL,
+    salt VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE posts (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    author_id INTEGER NOT NULL REFERENCES users(id),
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+);
---
+++ server.js
@@ -0,0 +1,46 @@
+// Initialize main modules
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// Initialize secondary utilities
+const routes = require("./controllers");
+const sequelize = require(".config/connection");
+const helpers = require("./utils/helpers");
+const { urlencoded } = require("express");
+
+// Set up server
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 3600,
+    httpOnly: false,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ db: sequelize }),
+};
+
+// Link it
+app.use(session(sess));
+
+// Set up addons
+const hbs = exphbs.create({ helpers });
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server initialization complete. Server listening on ${PORT}`);
+  });
+});
