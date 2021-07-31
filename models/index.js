c9e58c89c78c2e4f3e9b7e68808b501531eeb400
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-07-31 19:29:01 -0400

    Model relationships added

--- models/index.js
+++ models/index.js
@@ -6,5 +6,18 @@
 // =============================================
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
+
+// Users can have many posts or comments
+User.hasMany(Post, { foreignKey: "author_id", onDelete: "CASCADE" });
+User.hasMany(Comment, { foreignKey: "author_id" });
+
+// posts belong to a single user, but can have many comments
+Post.belongsTo(User, { foreignKey: "author_id" });
+Post.hasMany(Comment, { foreignKey: "on_post" });
+
+// comments belong to one author and one post
+Comment.belongsTo(User, { foreignKey: "author_id" });
+Comment.belongsTo(Post, { foreignKey: "on_post" });
 
 module.exports = { User, Post, Comment };
