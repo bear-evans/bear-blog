3bde3926adcf6e8d00970d7ef511c8f8bb0834ef
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-08-06 13:16:33 -0400

    Added quick and dirty validation and error handling for signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,10 +5,37 @@ const signup = (function () {
   // grabs data from the form inputs and assembles them into
   // a data object
   function getData() {
-    let nameRaw = $("#name-signup").val().trim();
-    let emailRaw = $("#email-signup").val().trim();
-    let passRaw = $("#password-signup").val().trim();
-    let checkRaw = $("#password-check-signup").val().trim();
+    let nameRaw = $('#name-signup').val().trim();
+    let emailRaw = $('#email-signup').val().trim();
+    let passRaw = $('#password-signup').val().trim();
+    let checkRaw = $('#password-check-signup').val().trim();
+
+    // Quick and dirty checks to see if anything is empty
+    // this wasn't part of the project so it's okay if it's clunky
+    if (!nameRaw) {
+      empError('name');
+      return false;
+    }
+
+    if (!emailRaw) {
+      empError('email');
+      return false;
+    }
+
+    if (!passRaw) {
+      empError('pass');
+      return false;
+    }
+
+    if (!checkRaw) {
+      empError('check');
+      return false;
+    }
+
+    if (passRaw !== checkRaw) {
+      passError();
+      return false;
+    }
 
     // User data object
     // TODO: Add validation so we're not using the raw input
@@ -21,35 +48,90 @@ const signup = (function () {
     return data;
   }
 
+  // creates an error notification if a field is empty
+  function empError(err) {
+    let styling = 'color: red;';
+    switch (err) {
+      case 'name':
+        $('#error-name').html(
+          `<p style="${styling}">Display name cannot be blank.</p>`
+        );
+        $('#name-signup').addClass('is-invalid');
+        break;
+      case 'email':
+        $('#error-email').html(
+          `<p style="${styling}">Email cannot be blank.</p>`
+        );
+        $('#email-signup').addClass('is-invalid');
+        break;
+      case 'pass':
+        $('#error-pass').html(
+          `<p style="${styling}">Password cannot be blank.</p>`
+        );
+        $('#password-signup').addClass('is-invalid');
+        break;
+      case 'check':
+        $('#error-check').html(
+          `<p style="${styling}">Password check cannot be blank.</p>`
+        );
+        $('#password-check-signup').addClass('is-invalid');
+        break;
+    }
+  }
+
+  // creates an error notification if passwords don't match
+  function passError() {
+    let styling = 'color: red;';
+    $('#error-check').html(`<p style="${styling}">Passwords don't match.</p>`);
+    $('#password-check-signup').addClass('is-invalid');
+  }
+
+  // clears all error displays
+  function clearError() {
+    $('#error-name').html(``);
+    $('#error-email').html(``);
+    $('#error-pass').html(``);
+    $('#error-check').html(``);
+    $('#name-signup').removeClass('is-invalid');
+    $('#email-signup').removeClass('is-invalid');
+    $('#password-signup').removeClass('is-invalid');
+    $('#password-check-signup').removeClass('is-invalid');
+  }
+
   async function submit() {
     event.preventDefault();
+    clearError();
     let user = getData();
 
-    const response = await fetch("/api/users/signup", {
-      method: "POST",
+    // don't do anything if the get data didn't work.
+    if (!user) {
+      return;
+    }
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
       body: JSON.stringify({
         name: user.name,
         email: user.email,
         password: user.password,
       }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace('/');
     } else {
-      document.location.replace("/signup?error=DBErr");
+      document.location.replace('/signup?error=DBErr');
     }
   }
 
   function init() {
-    $("#signup-button").on("click", submit);
+    $('#signup-button').on('click', submit);
 
     const urlParams = window.location.search;
     const params = new URLSearchParams(urlParams);
 
-    if (params.has("error")) {
-      $("#error-box").toggleClass("hidden");
+    if (params.has('error')) {
+      $('#error-box').toggleClass('hidden');
     }
   }
 
